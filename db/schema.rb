beb419203335a26f8cb8544e00eda44fa474a3b5
matthewroby <matthewroby@TORWM043.local>
2017-05-07 18:47:26 -0400

    Default value for User rating float
    added default value

--- db/schema.rb
+++ db/schema.rb
@@ -31,19 +31,19 @@ ActiveRecord::Schema.define(version: 20170507223308) do
   add_index "games", ["winner_id"], name: "index_games_on_winner_id"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",     null: false
+    t.string   "encrypted_password",     default: "",     null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,      null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.float    "rating"
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
+    t.float    "rating",                 default: 1000.0
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
