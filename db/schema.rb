7e42b3fda9a0ce6bdc8f0295aa7dd3862e1cfcc1
matthewroby <matthewroby@TORWM043.local>
2017-05-07 18:35:58 -0400

    added rating to User model
    Float value for rating on ELO.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170507222730) do
+ActiveRecord::Schema.define(version: 20170507223308) do
 
   create_table "games", force: :cascade do |t|
     t.integer  "user_id"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20170507222730) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.float    "rating"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
